0e4326a13ac94a74ab2af78e4c00b89122d91209
Junior Lima <josealbertorodriguesjunior@gmail.com>
2017-11-11 02:26:06 -0200

    Inserting cards

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,12 +2,24 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Titulo from './titulo/titulo';
 import Navbar from './navbar/navbar';
+import Card from './card/card';
 
 let App = (
     <div className="main">
-        <Navbar titulo="React" cor="red"/>
+        <Navbar titulo="React" cor="blue"/>
         <div className="container">
             <Titulo />
+            <div className="row">
+                <div className="col m4">
+                    <Card/>
+                </div>
+                <div className="col m4">
+                    <Card/>
+                </div>
+                <div className="col m4">
+                    <Card/>
+                </div>
+            </div>
         </div>
     </div>
 );
