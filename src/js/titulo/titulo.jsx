17224b040b3db07fd54e04b0b6420e00d2a955bd
Junior Lima <josealbertorodriguesjunior@gmail.com>
2017-11-20 15:09:53 -0200

    state lifecycle with a realtime clock

--- src/js/titulo/titulo.jsx
+++ src/js/titulo/titulo.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class Titulo extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {hora: new Date()};
+    }
+
+    componentDidMount(){
+        this.horaID = setInterval(
+            ()=> this.atualizarSegundos(),
+            1000
+        );
+    }
+
+    atualizarSegundos(){
+        this.setState({hora: new Date()});
+    }
+
+    componentWillUnMount(){
+        clearInterval(this.horaID);
+    }
+
     render(){
-        return <h1>React Course</h1>;
+        return <h1>React Course {this.state.hora.toLocaleTimeString()}</h1>;
         }
     }
 
