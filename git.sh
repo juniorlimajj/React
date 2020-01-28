ea4f1b9df3f14138cd2c06da1e71677c542b7b3a
Junior Lima <jose.junior2@br.experian.com>
2020-01-28 10:13:10 -0300

    git.sh fixes

--- git.sh
+++ git.sh
@@ -3,4 +3,4 @@ git add .
 read -p "Enter your commit message: " COMMITMESSAGE
 git commit -m "${COMMITMESSAGE}"
 read -p "Enter your branch: " BRANCH
-git push origin BRANCH
+git push origin ${BRANCH}
