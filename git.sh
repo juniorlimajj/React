d54072c12785f0b184b2eda052005bb81c5e5b30
Junior Lima <jose.junior2@br.experian.com>
2020-01-28 10:41:26 -0300

    it works

--- git.sh
+++ git.sh
@@ -6,5 +6,5 @@ git add .
 read -p "Enter your commit message: " COMMITMESSAGE
 git commit -m "${COMMITMESSAGE}"
 CURRENT="$(git rev-parse --abbrev-ref HEAD)"
-read -p "Enter your branch: (Your current branch is ${CURRENT})" BRANCH
+read -p "Enter your branch: (Your current branch is ${CURRENT})  " BRANCH
 git push origin ${BRANCH}
